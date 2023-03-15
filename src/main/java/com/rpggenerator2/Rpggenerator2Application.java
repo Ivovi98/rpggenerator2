782286b103e0d4ababb9ce23422efc537463b272
Ivovi98 <ivanfedorov1509@gmail.com>
2023-03-15 12:54:22 +0100

    Initial commit

---
+++ src/main/java/com/rpggenerator2/Rpggenerator2Application.java
@@ -0,0 +1,13 @@
+package com.rpggenerator2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rpggenerator2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rpggenerator2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpggenerator2/Rpggenerator2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpggenerator2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rpggenerator2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
